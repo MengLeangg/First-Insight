9abd6f3fab337bfe0728e943a1cb476140169fec
Mengleangg <mengleangtuong.it@gmail.com>
2019-12-08 13:36:56 +0700

    v.1

---
+++ forest-time/js/owlCarousel.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+    $('#partner-carousel').owlCarousel({
+    loop:true,
+    autoplay:true,
+    margin:10,
+    nav:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true,
+    smartSpeed: 450,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:4
+        }
+    }
+    });
+
+    $('#lastest-carousel').owlCarousel({
+    loop:true,
+    autoplay:true,
+    margin:0,
+    nav:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true,
+    smartSpeed: 450,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:4
+        }
+    }
+    });
+});
+
+// Slider 
